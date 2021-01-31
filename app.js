7b4754f0918baf06edd012b5793d247819cacb5a
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2021-01-31 11:55:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// Express app
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<html>Hello</html>');
+});
+
+app.listen(4000);
